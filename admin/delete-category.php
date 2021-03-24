d9ae66b685d5a3300880319b4faf7f6903f6d791
JoaoCoel <78794203+JoaoCoel@users.noreply.github.com>
2021-03-24 20:29:24 +0000

    apagar categorias e tipos e as suas relações

--- admin/delete-category.php
+++ admin/delete-category.php
@@ -3,6 +3,8 @@ include_once ("includes/body.inc.php");
 $id=intval($_GET['id']);
 $sql= "delete from categorias where categoriaId=".$id;
 mysqli_query($con,$sql);
+$sql= "delete from tipoCategorias where tipoCategoriaCategoriaId=".$id;
+mysqli_query($con,$sql);
 header("location:category-list.php");
 
 ?>
